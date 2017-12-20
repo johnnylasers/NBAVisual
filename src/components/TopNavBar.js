78be15f02f3b107e21b4e15f7ed1ce9bb3a482a3
johnnylasers <jianchen85@gmail.com>
2017-12-19 22:51:09 -0800

    added profile region

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -7,7 +7,7 @@ export class TopNavBar extends React.Component {
         return(
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <h1 className="App-title">Welcome to React</h1>
+                <h1 className="App-title"></h1>
             </header>
         );
     }
